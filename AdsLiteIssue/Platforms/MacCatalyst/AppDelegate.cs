7ddd0405f86fdcb5037515cb46abb79e9dacc223
Sergio Lillo <sergio.lillo@rethinkmarketing.es>
2022-04-20 11:36:24 +0200

    Add project files.

---
+++ AdsLiteIssue/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AdsLiteIssue;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
